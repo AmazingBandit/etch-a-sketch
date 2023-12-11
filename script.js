3fa823996ce1da5ea37da2c69c958c3297600970
sagun-mac <sagun-mac@sagun-macs-MacBook-Pro.local>
2023-12-11 15:16:56 -0800

    Added logic to rainbow button to enable random color while moving over square

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const container = document.querySelector(".container");
 let defaultGridSize = 16;
+let rainbow = false;
 
 //loop that creates div 16x16
-const createGrid = (x) => {
-  for (let i = 0; i < x ** 2; i++) {
+const createGrid = (gridSize, rainbow) => {
+  for (let i = 0; i < gridSize ** 2; i++) {
     let childDiv = document.createElement("div");
     //adds flex size for pixel and changes the pixel based on user input
-    childDiv.style.flex = `1 1 calc(100%/${x})`;
+    childDiv.style.flex = `1 1 calc(100%/${gridSize})`;
     container.append(childDiv);
 
-    //on mouse over changes color to red
+    //on mouse over changes color
     childDiv.addEventListener("mousemove", () => {
-      childDiv.style.backgroundColor = "green";
+      //Check if rainbow button was pressed
+      if (rainbow == true) {
+        childDiv.style.backgroundColor = `rgb(${Math.floor(
+          Math.random() * 256
+        )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+          Math.random() * 256
+        )})`;
+      } else {
+        childDiv.style.backgroundColor = "#202124";
+      }
     });
   }
 };
 
-//New Grid button asks user for grid size
-const newGrid = document.querySelector(".newGrid");
-newGrid.addEventListener("click", () => {
+const deleteGrid = (rainbow) => {
   let gridSize;
   const askUser = () => {
     gridSize = prompt("Enter a number per side for new grid: ");
@@ -42,7 +50,20 @@ newGrid.addEventListener("click", () => {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
-  createGrid(gridSize);
+  createGrid(gridSize, rainbow);
+};
+
+//New Grid button asks user for grid size
+const newGrid = document.querySelector(".newGrid");
+newGrid.addEventListener("click", () => {
+  deleteGrid();
 });
 
-createGrid(defaultGridSize);
+//Rainbow button makes pixel you hover over random colors
+const rainbowBtn = document.querySelector(".rainbowBtn");
+rainbowBtn.addEventListener("click", () => {
+  rainbow = true;
+  deleteGrid(rainbow);
+});
+
+createGrid(defaultGridSize, rainbow);
